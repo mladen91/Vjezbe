07a24b49199fd4aaf16733dfb95da4f3303596bd
mladen91 <mladen.teofilovic@bitcamp.ba>
2015-06-09 10:09:49 +0200

    bla bla

--- W3D5/src/Predavanja/Nplus1.java
+++ W3D5/src/Predavanja/Nplus1.java
@@ -2,19 +2,23 @@ package Predavanja;
 
 public class Nplus1 {
 
-	public static void threeNPlusOne(int n) {
+	public static String threeNPlusOne(int n) {
 
+		String str = " ";
 		int count = 1;
 		while (n > 1) {
 
-			if (n % 2 == 1)
+			if (n % 2 == 1) {
 				n = 3 * n + 1;
-			else
+			}else
 				n = n / 2;
 			count++;
-			System.out.println(n);
+			str += n;
 		}
-		System.out.println("Terms " + count);
+
+		//System.out.println("Terms " + count);
+		return str;
+
 	}
 
 	public static void main(String[] args) {
